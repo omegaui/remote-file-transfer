3084dbbf34300b3298b4cc74c65b029743eda0fa
omegaui <omegaui22@gmail.com>
2022-03-06 11:33:32 +0000

    Updated pull_down.sh

--- pull_down.sh
+++ pull_down.sh
@@ -2,6 +2,9 @@
 
 echo "Downloading client and server scripts"
 
+rm remote-file-receiver.py
+rm remote-file-sender.py
+
 wget https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/remote-file-receiver.py
 wget https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/remote-file-sender.py
 
