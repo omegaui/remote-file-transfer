ec5335cff2634fe41625813a4973444bff959c75
omegaui <omegaui22@gmail.com>
2022-03-06 11:15:28 +0000

    Updated pull_down.sh to use wget

--- pull_down.sh
+++ pull_down.sh
@@ -2,8 +2,8 @@
 
 echo "Downloading client and server scripts"
 
-curl https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/.release -o remote-file-sender.py
-curl https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/.release -o remote-file-receiver.py
+wget https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/remote-file-receiver.py
+wget https://raw.githubusercontent.com/omegaui/remote-file-transfer/main/remote-file-sender.py
 
 echo "Done!"
 
